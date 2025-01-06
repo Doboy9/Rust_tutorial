e017ad7304c6ed306d14b3d90da1b418025cc39c
dboire <dorian.boire98@gmail.com>
2025-01-06 17:56:28 +0100

    Feat : add Rectangle struct and area calculation in main function

--- Chapter4/Chapter5/rectangles/src/main.rs
+++ Chapter4/Chapter5/rectangles/src/main.rs
@@ -1,3 +1,18 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u16,
+    length: u16,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rect1 = Rectangle {
+        width: 30,
+        length: 50,
+    };
+    println!("rect1 is {rect1:?}");
+    println!("The area of the rectangle is {}", area(&rect1));
+}
+
+fn area(rectangle: &Rectangle) -> u16 {
+    rectangle.width * rectangle.length
 }
