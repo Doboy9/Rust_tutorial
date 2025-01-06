9b81fe70ff301a5d2cc8b931057f1b28e3681e47
dboire <dorian.boire98@gmail.com>
2025-01-06 12:41:37 +0100

    Feat : add Cargo configuration and initial main file for hello_cargo project

---
+++ Chapter1/hello_cargo/target/release/deps/hello_cargo-5d87010d1c3d6827.d
@@ -0,0 +1,5 @@
+/home/dboire/Projects/Rust_tutorial/Chapter1/hello_cargo/target/release/deps/hello_cargo-5d87010d1c3d6827: src/main.rs
+
+/home/dboire/Projects/Rust_tutorial/Chapter1/hello_cargo/target/release/deps/hello_cargo-5d87010d1c3d6827.d: src/main.rs
+
+src/main.rs:
---
+++ Chapter1/hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/dboire/Projects/Rust_tutorial/Chapter1/hello_cargo/target/release/hello_cargo: /home/dboire/Projects/Rust_tutorial/Chapter1/hello_cargo/src/main.rs
