9bd332b0b4459c8b76b2ef902c1bb1697bc19660
dboire <dorian.boire98@gmail.com>
2025-01-07 11:55:36 +0100

    Feat : update Rectangle struct to include height and add area and can_hold methods

--- Chapter4/Chapter5/rectangles/src/main.rs
+++ Chapter4/Chapter5/rectangles/src/main.rs
@@ -1,18 +1,44 @@
 #[derive(Debug)]
 struct Rectangle {
     width: u16,
-    length: u16,
+    height: u16,
+}
+
+impl Rectangle {
+    // We do not want to take ownership
+    fn area(&self) -> u16 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.area() > other.area()
+    }
+
+    fn square(size: u16) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
     let rect1 = Rectangle {
         width: 30,
-        length: 50,
+        height: 50,
     };
-    println!("rect1 is {rect1:?}");
-    println!("The area of the rectangle is {}", area(&rect1));
-}
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
 
-fn area(rectangle: &Rectangle) -> u16 {
-    rectangle.width * rectangle.length
+    let sq = Rectangle::square(3);
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("Square {:#?}", sq);
 }
